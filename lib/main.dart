2626cb0dcda7defd458ead532e6010c465ef7605
AlihanCelik <alihan_08032003@hotmail.com>
2025-02-03 17:37:44 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,9 @@ import 'package:zoom_clone/app/app.dialogs.dart';
 import 'package:zoom_clone/app/app.locator.dart';
 import 'package:zoom_clone/app/app.router.dart';
 import 'package:stacked_services/stacked_services.dart';
+import 'package:zoom_clone/resources/AuthMethods.dart';
+import 'package:zoom_clone/ui/views/home/home_view.dart';
+import 'package:zoom_clone/ui/views/login/login_view.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,6 +24,17 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: StreamBuilder(stream: AuthMethods().authChanges, builder: (context,snapshot){
+        if(snapshot.connectionState==ConnectionState.waiting){
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        if(snapshot.hasData){
+          return const HomeView();
+        }
+        return const LoginView();
+      }),
       initialRoute: Routes.startupView,
       onGenerateRoute: StackedRouter().onGenerateRoute,
       navigatorKey: StackedService.navigatorKey,
